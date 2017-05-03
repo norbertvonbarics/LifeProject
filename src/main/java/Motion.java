2962d0c5fb8fa4f86c37c14b0c3ab7c0f44da9e2
norbert barics <norbert.barics@gmail.com>
2017-05-03 17:30:22 +0200

    refactored motion class

--- src/main/java/Motion.java
+++ src/main/java/Motion.java
@@ -5,12 +5,12 @@ class Motion {
   static void motion() {
     for (int i = 0; i < Board.array2d().length; i++) {
       for (int j = 0; j < Board.array2d().length; j++) {
-        if ((Neighbour.checkNeighbours(i, j) < 2) && Alive.isAlive(i, j)) {
-          Board.setDead(i, j);
-        } else if ((Neighbour.checkNeighbours(i, j) > 3) && Alive.isAlive(i, j)) {
-          Board.setDead(i, j);
-        } else if ((Neighbour.checkNeighbours(i, j) == 3) && !Alive.isAlive(i, j)) {
+        if (Neighbour.checkNeighbours(i, j) == 3) {
           Board.setAlive(i, j);
+        } else if (Neighbour.checkNeighbours(i, j) < 2) {
+          Board.setDead(i, j);
+        } else if (Neighbour.checkNeighbours(i, j) > 3) {
+          Board.setDead(i, j);
         }
       }
     }
