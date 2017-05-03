823f1313fc4184aa83ba9605ba1ce1536d152670
norbert barics <norbert.barics@gmail.com>
2017-05-03 21:08:10 +0200

    repaired iteration on lifecycle

--- src/main/java/Motion.java
+++ src/main/java/Motion.java
@@ -1,15 +1,47 @@
+
+
 class Motion {
 
   static int counter = 0;
 
-  static void motion() {
+  private static int[][] toLive = new int[Board.size][Board.size];
+  private static int[][] toDead = new int[Board.size][Board.size];
+
+  static void makeThemLive() {
     for (int i = 0; i < Board.array2d().length; i++) {
       for (int j = 0; j < Board.array2d().length; j++) {
         if (Neighbour.checkNeighbours(i, j) == 3) {
-          Board.setAlive(i, j);
+          toLive[i][j] = 1;
+        } else {
+          toLive[i][j] = 0;
+        }
+      }
+    }
+    //return toLive;
+  }
+
+  static void makeThemDead() {
+    for (int i = 0; i < Board.array2d().length; i++) {
+      for (int j = 0; j < Board.array2d().length; j++) {
+        if (Neighbour.checkNeighbours(i, j) > 3) {
+          toDead[i][j] = 0;
         } else if (Neighbour.checkNeighbours(i, j) < 2) {
-          Board.setDead(i, j);
-        } else if (Neighbour.checkNeighbours(i, j) > 3) {
+          toDead[i][j] = 0;
+        } else {
+          toDead[i][j] = 1;
+        }
+      }
+    }
+  //  return toDead;
+  }
+
+  static void motion() {
+    for (int i = 0; i < Board.array2d().length; i++) {
+      for (int j = 0; j < Board.array2d().length; j++) {
+        if (toLive[i][j] == 1 && toDead[i][j] == 1) {
+          Board.setAlive(i, j);
+        }
+        if (toLive[i][j] == 0 && toDead[i][j] == 0) {
           Board.setDead(i, j);
         }
       }
